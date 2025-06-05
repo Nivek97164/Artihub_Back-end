7f8120111b7a64a25e6c8d1e9129960f4931bff6
Nivek97164 <kevinbernard971@gmail.com>
2025-06-05 15:19:15 +0200

    back +

--- auth/logout.php
+++ auth/logout.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: http://https://artihubfront-end-production-3c59.up.railway.app");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(200); exit; }
--- middleware/auth_required.php
+++ middleware/auth_required.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: http://https://artihubfront-end-production-3c59.up.railway.app");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json");
 
